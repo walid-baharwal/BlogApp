0c60a277eb2c8a2a58f98e243b9bef15026c1f44
Walid Baharwal <walidbaharwal5400@gmail.com>
2024-01-14 19:44:38 +0500

    changing title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,13 +17,13 @@ const Home = () => {
       <Container>
         <div className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
           <p className="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
-            Welcome to Our Blog Platform
+            Welcome to Our Blog APP
           </p>
           <p className="max-w-4xl text-base text-gray-600 md:text-xl">
             Explore a world of knowledge and inspiration. Discover insightful
             blog posts and articles.
           </p>
-          <div className="mt-6 flex w-full items-center space-x-2 md:w-1/3">
+          {/* <div className="mt-6 flex w-full items-center space-x-2 md:w-1/3">
             <input
               className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
               type="text"
@@ -35,7 +35,7 @@ const Home = () => {
             >
               Search
             </button>
-          </div>
+          </div> */}
         </div>
       </Container>
       <Blogs AllQueries={AllQueries} />
