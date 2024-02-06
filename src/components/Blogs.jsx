c55cf6bd18d9c16428c40a818137219303b1e906
Walid Baharwal <walidbaharwal5400@gmail.com>
2024-02-06 15:21:53 +0500

    added TINYMCE Api and added footer component

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -128,11 +128,11 @@ const Blogs = React.memo(function Blogs({AllQueries}) {
           <Loader size="xl" parentHeight="h-[60vh]" />
         ) : (
           <div className="grid gap-6 gap-y-10 py-6 sm:grid-cols-2 lg:grid-cols-3">
-            {blogs?.map((blog) => (
+            { blogs.lenght > 0 ? ( blogs?.map((blog) => (
               <Link key={blog.title} to={`/blog/${blog.$id}`}>
                 <BlogCard {...blog}  />
               </Link>
-            ))}
+            )) ) : (<h2>You have&apos;t posted anything yet</h2> )}
           </div>
         )}
       {/* </div> */}
