08d4ba6dea72448c302a16a307ac73a710d42d81
walid-baharwal <walidbaharwal5400@gmail.com>
2025-04-10 18:02:05 +0500

    outbound click

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -41,20 +41,9 @@ const Footer = () => {
           <div className="w-auto p-8">
             <div className="-m-1.5 flex flex-wrap">
               <div className="w-auto p-1.5">
-                <a href="#">
-                  <div className="flex h-8 w-8 items-center justify-center rounded-full border border-gray-300 hover:border-gray-400">
-                    <svg
-                      width="8"
-                      height="14"
-                      viewBox="0 0 8 14"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M5.55736 5.2L5.55736 3.88C5.55736 3.308 5.69631 3 6.66894 3H7.87315V0.800003L6.02052 0.800003C3.70473 0.800003 2.77841 2.252 2.77841 3.88V5.2H0.925781L0.925781 7.4H2.77841L2.77841 14H5.55736L5.55736 7.4H7.59526L7.87315 5.2H5.55736Z"
-                        fill="#27272A"
-                      ></path>
-                    </svg>
+                <a href="https://marketingplatform.google.com/about/tag-manager/">
+                  <div className="flex h-8 px-4  items-center justify-center rounded-full border border-gray-300 hover:border-gray-400">
+                 Google tag manager
                   </div>
                 </a>
               </div>
